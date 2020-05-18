1637246ec6a49ac74c93740892759f0e80260df1
knottass <knottassoc@yahoo.com>
2020-05-17 20:06:21 -0600

    Initial commit

---
+++ .idea/DeskLamp.java
@@ -0,0 +1,18 @@
+class Desk Lamp {
+
+}
+	public class DeskLamp{
+	
+	// instance variable
+	private boolean isOn;
+	
+	// method
+	public void turnOn() {
+	isOn = true;
+}
+
+	// method
+	public void turnOff() {
+	isOn = false;
+	}
+}
